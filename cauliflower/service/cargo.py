43fac23cafa6bc92380cb6bd5ce500ae13195c6d
stefanooldeman <stefano.oldeman@gmail.com>
2013-03-24 22:23:53 +0100

    CargoService checks the incomming file

--- cauliflower/service/cargo.py
+++ cauliflower/service/cargo.py
@@ -1,6 +1,5 @@
-import xml.sax
-import json
 import os
+import mimetypes
 
 from cauliflower.domain.mappers.pattern import PatternXMLMapper
 import cauliflower.errors as errors
@@ -9,17 +8,29 @@ import cauliflower.errors as errors
 class CargoService(object):
 
     def __init__(self, basepath):
+        if basepath[-1] is not os.sep:
+            # if last char (-1) is not a slash (/) append it
+            basepath += os.sep
+
         self.upload_dir = basepath
 
     def do_import(self, filepath):
-        if self._checkfile(filepath):
-            raise errors.ValidationError('file not supported')
+        if self._checkfile(self.upload_dir + filepath) is False:
+            raise errors.ValidationError('filetype is not supported')
+
         mapper = PatternXMLMapper()
-        mapper.toEntity(row)
+        mapper.toEntity('')
+        self._size = 0
+        return True
 
     def do_export(self, desitnation):
         raise NotImplemented
 
     def _checkfile(self, filepath):
-        supported = ['xml', 'json']
-        return True
+        open(filepath, 'r')
+        (filetype, _) = mimetypes.guess_type(filepath)
+        return bool(filetype.split('/')[1] in ['xml', 'json'])
+
+    @property
+    def size(self):
+        return self._size
--- cauliflower/tests/service/test_cargo.py
+++ cauliflower/tests/service/test_cargo.py
@@ -2,17 +2,29 @@ import unittest
 import os
 
 from cauliflower.service.cargo import CargoService
+import cauliflower.errors as errors
 
 
 class TestCargo(unittest.TestCase):
 
     def setUp(self):
-        thedir = os.path.abspath(os.curdir + "../mocks/uploads/")
+        p = os.path
+        thedir = p.abspath(p.dirname(__file__) + "/../mocks/uploads/")
         print thedir
-        self.my_upload_dir = thedir
+        self.cargo = CargoService(thedir)
 
     def test_import(self):
-        cargo = CargoService(self.my_upload_dir)
-        filename = 'dummy_import.xml'
-        cargo.do_import(filename)
-        self.assertEqual(cargo.size, 3)
+        self.cargo.do_import('dummy_import.xml')
+        self.assertEqual(self.cargo.size, 3)
+
+    def test_do_import_valid_filetypes_returns_true(self):
+        self.assertTrue(self.cargo.do_import('dummy_import.xml'))
+        self.assertTrue(self.cargo.do_import('dummy_import.json'))
+
+    def test_do_import_unsupported_filetypes_raises(self):
+        with self.assertRaises(errors.ValidationError):
+            self.cargo.do_import('dummy_import.doc')
+
+    def test_do_import_screams_filenotfound(self):
+        with self.assertRaises(IOError):
+            self.assertTrue(self.cargo._checkfile('rickrolled.xml'))
