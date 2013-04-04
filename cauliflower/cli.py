aad2060cddbe7e1aaf160f68845b534c910252e8
stefanooldeman <stefano.oldeman@gmail.com>
2013-04-04 09:49:59 +0200

    make it follow PEP8

--- cauliflower/cli.py
+++ cauliflower/cli.py
@@ -1,30 +1,23 @@
 import argparse
 from cauliflower.service.cargo import CargoService
 
+
 def multi_input(msg):
-	print msg
-	text = ""
-	stopword = "Q"
-	while True:
-		line = raw_input()
-		if line.strip() == stopword:
-			break
-		text += "%s\n" % line
-	return text
+    print msg
+    text = ""
+    stopword = "Q"
+    while True:
+        line = raw_input()
+        if line.strip() == stopword:
+            break
+        text += "%s\n" % line
+    return text
 
 print "We are going to add a Pattern!"
-#Pattern name 
-s1 = raw_input ("Please add Pattern Name: ")
-print "The " ,s1, " Pattern has been added!"
-#Pattern definition
-s2 = multi_input("Please add the {pattern_name} problem description. Type Q to end input.".format(pattern_name=s1))
-print "You added the following problem discription to the " +s1+ " Pattern:\n" + s2
-#Where to use the pattern
-s3 = multi_input("Please describe the solution {pattern_name} is solving. Type Q to end input.".format(pattern_name=s1))
-print s3
-#
-s3 = multi_input("Please describe the solution {pattern_name} is solving. Type Q to end input.".format(pattern_name=s1))
-print s3
-
-#c = CargoService('/media/uploads/')
-#c.do_import('foobar.xml')
+name = raw_input("Pattern Name: ")
+problem = multi_input("Add the problem description for {pattern_name} . \
+        Type Q to end input.".format(pattern_name=s1))
+solution = multi_input("Describe the solution {pattern_name} is solving. \
+        Type Q to end input.".format(pattern_name=s1))
+consequence = multi_input("Describe the consequence of using {pattern_name}. \
+        Type Q to end input.".format(pattern_name=s1))
