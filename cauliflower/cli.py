ec8e0b15cf74eaabca8260467cd29f83e0075eee
stefanooldeman <stefano.oldeman@gmail.com>
2013-04-10 09:36:17 +0200

    add function handler for pattern

--- cauliflower/cli.py
+++ cauliflower/cli.py
@@ -8,7 +8,7 @@ commands = [
             "description": "Cargo can get you a bunch of data from point A to B",
             "help": "use this to import or export data"
         }),
-        ("patterns", {
+        ("pattern", {
             "description": "",
             "help": "modify or add patterns in interactive mode"
         })
@@ -34,32 +34,19 @@ options = {
     "pattern": [
         ('-name', {
             "help": "the name of the pattern",
-            "required": True
+            "required": True,
+            "type": str
             }
         ),
         ("--add", {
-            "help": "add a pattern in interaction session",
+            "help": "boolean flag, add a pattern in interaction session",
+            "const": True,
+            "default": False,
+            "nargs": "?",
             })]
 }
 
-def add_commands(parser):
-
-    subparser = parser.add_subparsers(title="The most commonly used cauliflower commands are:",
-                                    help="see cauliflower <command> -h for more information",
-                                    description="")
-
-    subs_commands = dict()
-    for (name, construct_args) in commands:
-        cmd = subparser.add_parser(name, **construct_args)
-
-        for (cmd_option, option_args) in options.get(name, []):  # default to empty options
-            cmd.add_argument(cmd_option, **option_args)
-            subs_commands[name] = cmd
-
-    return parser
-
-
-def add_pattern():
+def add_pattern(name):
 
     def multi_input(msg):
         print msg
@@ -72,11 +59,39 @@ def add_pattern():
             text += "%s\n" % line
         return text
 
-    print "We are going to add a Pattern!"
-    name = raw_input("Pattern Name: ")
+    print "We are going to add the {} Pattern!".format(name)
     problem = multi_input("Add the problem description for {pattern_name} . \
-            Type Q to end input.".format(pattern_name=s1))
+            Type Q to end input.".format(pattern_name=name))
     solution = multi_input("Describe the solution {pattern_name} is solving. \
-            Type Q to end input.".format(pattern_name=s1))
+            Type Q to end input.".format(pattern_name=name))
     consequence = multi_input("Describe the consequence of using {pattern_name}. \
-            Type Q to end input.".format(pattern_name=s1))
+            Type Q to end input.".format(pattern_name=name))
+
+def pattern_handler(args):
+    if args.add:
+        add_pattern(args.name)
+    #print args
+
+
+def add_commands(parser):
+    subparser = parser.add_subparsers(title="The most commonly used cauliflower commands are:",
+                                    help="see cauliflower <command> -h for more information",
+                                    description="")
+
+    # define by which functions a cmd should be handled if aplicable
+    hanlders = {
+            "pattern": pattern_handler
+            }
+
+    subs_commands = dict()
+    for (name, construct_args) in commands:
+        cmd = subparser.add_parser(name, **construct_args)
+
+        if name in hanlders:
+            cmd.set_defaults(func=hanlders.get(name))
+
+        for (cmd_option, option_args) in options.get(name, []):  # default to empty options
+            cmd.add_argument(cmd_option, **option_args)
+        subs_commands[name] = cmd
+
+    return parser
